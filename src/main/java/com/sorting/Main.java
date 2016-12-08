29b4f601c289eae00e8f6f01abef1f269e7996a9
ooliinyk <ooliinyk@outlook.com>
2016-12-08 13:03:41 +0200

    12

--- src/main/java/com/sorting/Main.java
+++ src/main/java/com/sorting/Main.java
@@ -26,7 +26,7 @@ public class Main {
 //        System.out.println(myList1.toString());
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+ " ");
+            System.out.print(arr[i]+ "  ");
 
         }
 //        for (int i = 0; i < arr.length ; i++) {
